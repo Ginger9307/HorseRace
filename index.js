221b69208f64e90a04b4c8924abdc3bbe1147cec
“Ginger9307” <tvlezhneva@gmail.com>
2021-11-30 22:39:30 +0000

    button for list

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-function newRace() {
-    horses = ["one", "two", "three", "four"];
-    horsesOutput = [];
-    for (i=0; i<horses.lengh; i++) {
-        horsesOutput.push(horses[i] + <br/>)
-    }
-    return horsesOutput;
-}
-document.getElementById("names").innerHTML = newRace;
-document.getElementById("test").innerHTML = "Test"
+function nameGenerator() {
+    let Moniker = require('moniker');
+    // console.log(Moniker.choose());
+    // let generateName = require('sillyname');
+    // let sillyName = generateName();
+    alert(Moniker.choose());
+    // return sillyName;
+  }
+
+  function newRace() {
+      horses= [];
+      // horses = ["one", "two", "three", "four"];
+      nameGenerator();
+      for (i=0; i<3; i++) {
+        horses.push(nameGenerator());
+        alert(horses[i]);
+      }
+
+      horses.forEach(element => {
+        alert(`horse${horses.indexOf(element)}`)
+        document.getElementById(`horse${horses.indexOf(element)+1}`).innerHTML = element;
+       });
+  }    
+
+
+  document.getElementById("button1").onclick = newRace;
+  
